82de2a67002f9102e35bd0587fd85c42f2e18f36
Reasondro <alessandrohasian16@gmail.com>
2024-04-27 19:02:39 +0700

    prep for Decks/Inventory

---
+++ app/src/main/java/Deck/Deck.java
@@ -0,0 +1,15 @@
+package Deck;
+
+import java.util.*;
+
+public class Deck<T> {
+
+    //TODO implement Deck class with a list of plants/cards
+    
+    List<T> myDeck = new ArrayList<T>(10);
+
+
+
+
+    //TODO add swap methods
+}
