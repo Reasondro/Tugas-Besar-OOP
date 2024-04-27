a6f6ada1d8c3ea44b466091d8f99293439d240bb
Reasondro <alessandrohasian16@gmail.com>
2024-04-27 18:03:00 +0700

    removed redundant folders

---
+++ app/src/main/java/DLC/Semester.java
@@ -0,0 +1,7 @@
+package DLC;
+
+public class Semester 
+
+{
+    
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,4 +11,4 @@ plugins {
 }
 
 rootProject.name = "Tugas-Besar-OOP"
-include("app", "list", "utilities")
+include("app", "list", "Plants", "Zombie", "Game")
