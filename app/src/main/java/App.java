2307f294d75dc08ef9285a7e14280312cc0a64d6
Reasondro <alessandrohasian16@gmail.com>
2024-05-02 22:33:10 +0700

    made GameMap implementation

--- app/src/main/java/App.java
+++ app/src/main/java/App.java
@@ -2,39 +2,47 @@ import Plants.*;
 import Zombies.*;
 import Sun.*;
 import java.util.*;
+import Petak.*;
+import Position.*;
+
+import GameMap.*;
 public class App { 
     public static void main(String[] args)  {
 
-        // Lilypad lilypad = new Lilypad();
-        // lilypad.displayStatus();
-
-        Squash p = new Squash();
-        p.displayStatus();
+        // Squash p = new Squash();
+        // p.displayStatus();
         
-        NormalZombie z = new NormalZombie();
-        z.displayStatus();
+        // NormalZombie z = new NormalZombie();
+        // z.displayStatus();
 
-        System.out.println("Zombie Health :" + z.getHealth());
-        p.addTarget(z);
-        p.useAbility();
-        System.out.println("Zombie Health :" + z.getHealth());
+        // System.out.println("Zombie Health :" + z.getHealth());
+        // p.addTarget(z);
+        // p.useAbility();
+        // System.out.println("Zombie Health :" + z.getHealth());
 
-        //? Yes, correct zombinya masi bisa attack padahal sudah dead. SEKEDAR testing untuk sementara, implementasi nanti akan diubah
+        // //? Yes, correct zombinya masi bisa attack padahal sudah dead. SEKEDAR testing untuk sementara, implementasi nanti akan diubah
 
-        System.out.println("Plant Health:" + p.getHealth());
-        z.attackPlant(p);
-        System.out.println("Plant Health:" + p.getHealth());
+        // System.out.println("Plant Health:" + p.getHealth());
+        // z.attackPlant(p);
+        // System.out.println("Plant Health:" + p.getHealth());
 
-        Sun sun = new Sun();
-        sun.displayStatus();
+        // Sun sun = new Sun();
+        // sun.displayStatus();
 
-        Sunflower sunflower = new Sunflower();
-        sunflower.useAbility();
+        // Sunflower sunflower = new Sunflower();
+        // sunflower.useAbility();
 
-        sun.displayStatus();
-
-
-        // Game.main(args);
+        // sun.displayStatus();
+        // Petak petak = new Petak("Normal", new Position(0, 0));
+        // petak.printType();
+        // petak.addCreature(new NormalZombie());
+        // petak.addCreature(new Sunflower());
+        // petak.addCreature(new NormalZombie());
+        // petak.printCreature();
+        GameMap map = new GameMap();
+        // map.printMap();
+        // map.printTest();
+        map.printMap();
 
     }
 }
--- app/src/main/java/Petak/Petak.java
+++ app/src/main/java/Petak/Petak.java
@@ -8,14 +8,26 @@ import Creature.Creature;
 
 public class Petak {
 
+    private String type; //? type of petak (Protected, Normal, Pool, ZombieSpawnArea)
     private Position pos;
     private List<Creature> creatures;
-    private String type; //? type of petak (Protected, Normal, Pool, ZombieSpawnArea)
 
     public Petak(String type ,Position pos) {
-        this.pos = pos;
         this.type = type; //? TODO initialize pas generate map
+        this.pos = pos;
         this.creatures = new ArrayList<>();
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void printType() {
+        System.out.println("Petak type: " + type);
     }
 
     // Add a creature to the list
@@ -32,6 +44,33 @@ public class Petak {
     // Get all creatures in the petak
     public List<Creature> getCreatures() {
         return creatures;
+    }
+
+    public Position getPos() {
+        return pos;
+    }
+
+    public void setPos(Position pos) {
+        this.pos = pos;
+    }
+
+    public void printPos() {
+        System.out.println("Petak position: (Kolom = " + pos.getY() + ", Baris = " + pos.getX() + ")");
+    }
+    // public void printPos() {
+    //     System.out.println("Petak position: (Baris = " + pos.getX() + ", Kolom = " + pos.getY() + ")");
+    // }
+
+
+    public void printCreature() {
+        System.out.print("[");
+        for (int i = 0; i < creatures.size(); i++) {
+            System.out.print(creatures.get(i).getName());
+            if (i < creatures.size() - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println("]");
     }
 }
 
