548dba338d31f47aafb7f6a0b616dae351a9c53a
EleanorCordelia <elenever56@gmail.com>
2024-05-18 00:26:09 +0700

    fixing potatomine

--- app/src/main/java/Plants/PotatoMine.java
+++ app/src/main/java/Plants/PotatoMine.java
@@ -10,6 +10,7 @@ import java.util.ArrayList;
 
 public class PotatoMine extends Plant implements PlantAbility{
 
+    private boolean isReady = false;
     private List<Zombie> targets = new ArrayList<>();
     
     public PotatoMine()
@@ -17,6 +18,17 @@ public class PotatoMine extends Plant implements PlantAbility{
         super("PotatoMine", 25, 400, 180, 1, -1, 0,  new Position(0, 0)); // masih tentatif damage dia berapa
     }
     
+    @Override
+    public void refreshCreature() {
+        super.refreshCreature();
+        if (!isReady) {
+            if (getCooldown() == 0) {
+                isReady = true;
+                System.out.println("PotatoMine is ready to explode!");
+            }
+        }
+    }
+
     public void setTargets(List<Zombie> targets)
     {
         this.targets = targets;
@@ -32,17 +44,32 @@ public class PotatoMine extends Plant implements PlantAbility{
         targets.remove(z);
     }
 
+    // Override method from Plant class to handle using the ability
     @Override
-    public void useAbility( )
-    {
-        for(Zombie z : targets)
-        {
-           z.reduceHealth(z.getHealth()); //? instant kill
+        public void useAbility() {
+        if (isReady) {
+            for (Zombie z : targets) {
+                z.reduceHealth(z.getHealth()); // Menghabiskan health zombie (Instant kill)
+            }
+            // Set health of PotatoMine to 0 (Menghilangkan PotatoMine)
+            setHealth(0);
+            System.out.println("PotatoMine explodes and vanishes!");
+        } else {
+            System.out.println("PotatoMine is not ready yet!");
         }
     }
+
     @Override   
     public void checkToUseAbility(){}
 
-
 }
+
+/* jadi disini aku add variabel boolean isReady yang 
+menandakan apakah PotatoMine sudah siap untuk meledak atau belum. 
+Waktu siapnya diatur pas pertama kali dia ditanam dgn nilai cooldown pd konstruktornya. 
+Setelah itu, dalam setiap pembaruan (refreshCreature()), bakal diperiksa apakah 
+PotatoMine sudah siap atau belum. Kalau sudah, 
+dia akan meledak dan mengurangi hp zombie di depannya (yg jd target). 
+Setelah meledak, hp PotatoMine diatur jadi 0 yang show kalau dia udh ga aktif lagi
+*/
 
