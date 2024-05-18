ff0dddb02f9f7a13396556feede282cf4059547e
EleanorCordelia <elenever56@gmail.com>
2024-05-19 00:53:27 +0700

    fix piercer

--- app/src/main/java/Plants/Piercer.java
+++ app/src/main/java/Plants/Piercer.java
@@ -4,6 +4,7 @@ import Position.Position;
 
 import PlantAbility.*;
 import Zombies.Zombie;
+import Bullet.PeaBullet;
 import Bullet.PiercerBullet;
 
 import java.util.List;
@@ -14,22 +15,81 @@ import Petak.Petak;
 
 public class Piercer extends Plant implements PlantAbility{
     
+    private PiercerBullet bullet;
+    private List<Petak> reachablePetak = new ArrayList<Petak>();
+
     public Piercer()
     {
-        super("Piercer", 100, 100, 0, 0, 0, 0,  new Position(0, 0));   
-    }
+        super("Piercer", 100, 100, 0, 0, 0, 0,  new Position(0, 0));
+        bullet =  new PiercerBullet(getAttackDamage());
+    }   
     
-    public void specialSetPosition(Position newPosition) {
-        super.setPos(newPosition);
-        // Double the bullet damage of other plants in the same position
-        doubleOtherPlantsDamage(newPosition);
+
+    public List<Petak> getReachablePetak()
+    {
+        return reachablePetak;
     }
 
-    private void doubleOtherPlantsDamage(Position position) {
-        for (Plant plant : position.getPlants()) {
-            if (plant != this) {
-                plant.doubleBulletDamage();
+    public void setReachablePetak(List<Petak> reachablePetak)
+    {
+        this.reachablePetak = reachablePetak;
+    }
+
+    public boolean isZombiesInRange()
+    {
+        setReachablePetak(GameMap.getInstance().getRowBasedOnPlantRange(this));
+        for(Petak p : reachablePetak)
+        {
+            synchronized(p)
+            {
+                if(!(p.getZombies().isEmpty()))
+                {
+                    return true;
+                }
             }
+    }
+        return false;
+    }
+
+
+        @Override
+    public void useAbility()
+    {
+        for(Petak p : reachablePetak)
+            {
+                synchronized(p) //TODO add synchronized to objects that need(petak)
+                {
+                if(!(p.getZombies().isEmpty()))
+                {
+                    if(!(bullet.isWornOut()))
+                    {
+                    bullet.hit(p);
+                    }
+                    else
+                    {
+                        break;
+                    }     
+                }
+                }
+            }
+            setAttackTimer(getAttackSpeed()); 
+            bullet = new PiercerBullet(getAttackDamage());
+    }
+
+    @Override
+    public void checkToUseAbility()
+    {
+        if (isZombiesInRange() && getAttackTimer() == 0)
+        {
+            useAbility();
+        }
+        else if(getAttackTimer() > 0)
+        {
+            setAttackTimer(getAttackTimer()-1);
+        }
+        else if(!(isZombiesInRange()) && getAttackTimer() == 0)
+        {
+            System.out.printf("No zombies in range for %s\n", getName());
         }
     }
 }
