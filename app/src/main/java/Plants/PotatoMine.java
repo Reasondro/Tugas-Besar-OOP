36cc8ab148a06b515008ce38ae926cd47d686571
EleanorCordelia <elenever56@gmail.com>
2024-05-18 21:46:32 +0700

    fixing potatomine

--- app/src/main/java/Plants/PotatoMine.java
+++ app/src/main/java/Plants/PotatoMine.java
@@ -6,70 +6,66 @@ import PlantAbility.*;
 import Zombies.Zombie;
 
 import java.util.List;
+
+import GameMap.GameMap;
+import Petak.Petak;
+
 import java.util.ArrayList;
 
-public class PotatoMine extends Plant implements PlantAbility{
+public class PotatoMine extends Plant implements PlantAbility {
 
     private boolean isReady = false;
-    private List<Zombie> targets = new ArrayList<>();
+    private List<Petak> reachablePetak = new ArrayList<>();
     
-    public PotatoMine()
-    {
-        super("PotatoMine", 25, 400, 180, 1, -1, 0,  new Position(0, 0)); // masih tentatif damage dia berapa
+    public PotatoMine() {
+        super("PotatoMine", 25, 400, 180, 1, -1, 0, new Position(0, 0)); // Tentative damage
     }
     
+    // Uncomment and implement refreshCreature if needed
+    // @Override
+    // public void refreshCreature() {
+    //     super.refreshCreature();
+    //     if (!isReady) {
+    //         if (getCooldown() == 0) {
+    //             isReady = true;
+    //             System.out.println("PotatoMine is ready to explode!");
+    //         }
+    //     }
+    // }
+
+    public boolean isZombiesInSamePetak() {
+        return !GameMap.getInstance().getPetak(getPos()).getZombies().isEmpty();
+    }
+
+    public List<Petak> getReachablePetak() {
+        return reachablePetak;
+    }
+
+    public void setReachablePetak(List<Petak> reachablePetak) {
+        this.reachablePetak = reachablePetak;
+    }
+
+    // Override method from PlantAbility interface to handle using the ability
     @Override
-    public void refreshCreature() {
-        super.refreshCreature();
-        if (!isReady) {
-            if (getCooldown() == 0) {
-                isReady = true;
-                System.out.println("PotatoMine is ready to explode!");
+    public void useAbility() {
+        if (isReady && isZombiesInSamePetak()) {
+            Petak currentPetak = GameMap.getInstance().getPetak(getPos());
+            List<Zombie> zombies = currentPetak.getZombies();
+            for (Zombie z : zombies) {
+                z.reduceHealth(z.getHealth()); // Instant kill zombies --> zombies dies
             }
-        }
-    }
-
-    public void setTargets(List<Zombie> targets)
-    {
-        this.targets = targets;
-    }
-
-    public void addTarget(Zombie z)
-    {
-        targets.add(z);
-    }
-
-    public void removeTarget(Zombie z)
-    {
-        targets.remove(z);
-    }
-
-    // Override method from Plant class to handle using the ability
-    @Override
-        public void useAbility() {
-        if (isReady) {
-            for (Zombie z : targets) {
-                z.reduceHealth(z.getHealth()); // Menghabiskan health zombie (Instant kill)
-            }
-            // Set health of PotatoMine to 0 (Menghilangkan PotatoMine)
-            setHealth(0);
+            setHealth(0); // Set health of PotatoMine to 0 (remove PotatoMine cuz it's already exploded)
             System.out.println("PotatoMine explodes and vanishes!");
         } else {
             System.out.println("PotatoMine is not ready yet!");
         }
     }
 
-    @Override   
-    public void checkToUseAbility(){}
-
+    // Implement checkToUseAbility method from PlantAbility interface
+    @Override
+    public void checkToUseAbility() {
+        if (isReady && isZombiesInSamePetak()) {
+            useAbility();
+        }
+    }
 }
-
-/* jadi disini aku add variabel boolean isReady yang 
-menandakan apakah PotatoMine sudah siap untuk meledak atau belum. 
-Waktu siapnya diatur pas pertama kali dia ditanam dgn nilai cooldown pd konstruktornya. 
-Setelah itu, dalam setiap pembaruan (refreshCreature()), bakal diperiksa apakah 
-PotatoMine sudah siap atau belum. Kalau sudah, 
-dia akan meledak dan mengurangi hp zombie di depannya (yg jd target). 
-Setelah meledak, hp PotatoMine diatur jadi 0 yang show kalau dia udh ga aktif lagi
-*/
-
