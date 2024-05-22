dbf3934451b88f1b4fc3c5c230429383fb5ee753
EleanorCordelia <elenever56@gmail.com>
2024-05-22 13:21:29 +0700

    kernel

--- app/src/main/java/Bullet/KernelBullet.java
+++ app/src/main/java/Bullet/KernelBullet.java
@@ -6,63 +6,29 @@ import Zombies.Zombie;
 public class KernelBullet extends Bullet {
 
     private int damage;
-    private boolean stopzomb = false;
-    private boolean isButter = false; // Untuk melacak apakah peluru adalah mentega
-
-    private long lastShotTime; // TUntuk melacak waktu tembakan terakhir
 
     public KernelBullet(int damage) {
         super(damage);
-        this.lastShotTime = System.currentTimeMillis(); 
     }
 
-    public int getDamage() {
-        return damage;
-    }
+   public void hit(Petak p)
+    {
 
-    public void setDamage(int damage) {
-        this.damage = damage;
-    }
-
-    public boolean isStopZomb() {
-        return stopzomb;
-    }
-
-    public void setStopZomb(boolean stopzomb) {
-        this.stopzomb = stopzomb;
-    }
-
-    public boolean isButter() {
-        return isButter;
-    }
-
-    public void setButter(boolean isButter) {
-        this.isButter = isButter;
-    }
-
-    public void hit(Petak p) {
-        long currentTime = System.currentTimeMillis();
-        if ((currentTime - lastShotTime) >= 5000) { // Cek apakah 5 detik telah berlalu
-            setButter(true);
-            lastShotTime = currentTime; // Reset  timer
-        } else {
-            setButter(false);
-        }
-
-        for (Zombie z : p.getZombies()) {
-            if (isButter()) {
-                z.reduceHealth(getDamage()); // Apply damage
-                if (!z.isFrozen()) {
-                    z.setFrozen(true);
-                    z.setFrozenTimer(10); // Set frozen timer for 10 seconds
-                }
-                System.out.printf("Hit %s with butter causing freeze\n", z.getName());
-            } else {
-                z.reduceHealth(getDamage()); // Apply damage with corn kernel
-                System.out.printf("Hit %s with corn kernel causing damage %d\n", z.getName(), getDamage());
+        synchronized(p)
+        {
+           for(Zombie z : p.getZombies())
+           {
+            z.reduceHealth(getDamage());
+            z.setFrozenTimer(10);
+            if(z.isFrozen() == false)
+            {
+                z.setFrozen(true);
+                z.setWalkSpeedInSeconds(z.getWalkSpeedInSeconds() * 2);
             }
+           }
         }
+           setWornOut(true);
 
-        setStopZomb(true);
     }
+    
 }
