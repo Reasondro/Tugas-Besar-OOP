9186814e083d63137b730951023fe19ecb633c41
EleanorCordelia <elenever56@gmail.com>
2024-05-23 12:12:56 +0700

    Add customPlantBullet + customPlantFactory from eleanor branch

---
+++ app/src/main/java/PlantFactory/PiercerFactory.java
@@ -0,0 +1,35 @@
+package PlantFactory;
+
+import Plants.Plant;
+import Plants.Snowpea;
+import Plants.Kernelpult;
+import Plants.Piercer;
+
+public class PiercerFactory extends PlantFactory {
+
+    private static int   piercerCount = 0;
+
+    public PiercerFactory()
+    {
+        super("Piercer Card", 10,false, 120);     
+    
+
+    }
+
+    public Plant createPlant()
+    {
+        piercerCount++;
+        PlantFactory.incrementPlantCount();
+
+        setCooldownTimer(getFactoryCooldown());
+        setReady(false);
+
+        return new Kernelpult();
+    }
+
+    public static int getPiercerCount()
+    {
+        return   piercerCount;
+    }
+    
+}
